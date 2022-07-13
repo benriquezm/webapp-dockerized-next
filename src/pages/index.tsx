86045d2c2ac3c01310052f8d29ce02b597ea70bc
Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-07-12 23:52:58 -0500

    feat(setup-tailwind): Add setup for use tailwindcss.
    TODO
    
    check setup for tailwindcss is more optimum
    check add packages for tailwindcss is necessary

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const HomePage = () => {
-    return <div>Welcome to Next.js!, se está actualizando ...</div>;
+    return (
+        <div className="text-3xl font-bold underline">
+            Welcome to Next.js!, se está actualizando ...
+        </div>
+    );
 };
 
 export default HomePage;
---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: {
+        content: [
+            './pages/**/*.{js,ts,jsx,tsx}',
+            './components/**/*.{js,ts,jsx,tsx}',
+        ],
+        options: {
+            safelist: [],
+        },
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    darkMode: `class`,
+};
