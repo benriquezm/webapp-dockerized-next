a38a6d396ee43d6aef0ee68edc912cfd40c01f6a
Benito Enríquez Mora <enriquez.ico@gmail.com>
2022-07-15 22:25:37 -0500

    feat(setup-tailwindcss): add file _app.tsx config global css tailwindcss.
    TODO
    
    - configure google fonst Nunito font.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import LayoutPrincipal from '../components/layouts/layoutPrincipal';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <LayoutPrincipal>
+            <Component {...pageProps} />
+        </LayoutPrincipal>
+    );
 }
